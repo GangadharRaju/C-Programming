b4623e23ef73134c35b598755c1164d03720960b
Udayabhanu N P G Raju <unpgraju_cse@acoe.edu.in>
2021-05-24 13:11:27 +0530

    Add files via upload

---
+++ ATUOSTOR.C
@@ -0,0 +1,15 @@
+/* AUTO STORAGE CLASS DEMO */
+#include<stdio.h>
+void main()
+{
+ int a;
+ float b;
+ char e;
+ clrscr();
+ printf("PRINT DEFAULT VALUES OF A,B,C \n");
+ printf("********************************\n");
+ printf("%d\t %f\t %c\t",a,b,e);
+}
+
+
+
