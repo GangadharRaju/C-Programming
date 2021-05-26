acd257d75f898f4ff4c19e046356747567609391
Udayabhanu N P G Raju <unpgraju_cse@acoe.edu.in>
2021-05-26 13:06:38 +0530

    Add files via upload

---
+++ CONSTTES.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+ int marks;
+ const int passmark=40;
+ printf("\n Enter marks value:");
+ scanf("%d",&marks);
+ printf("The marks value is: %d\n",marks);
+ printf("The passmark value is: %d",passmark);
+}
